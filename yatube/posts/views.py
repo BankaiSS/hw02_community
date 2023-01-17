811740899ff24570f0d3293b08960dbc65d810a8
Байбанов Эльдар <baibanove@mail.ru>
2023-01-17 04:31:00 +0300

    2nd review fixes

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
+from yatube.settings import NUMBER_OF_POSTS
+
 
 def index(request):
-    posts = Post.objects.all()[:10]
-    title = "Последние обновления на сайте"
+    posts = Post.objects.all()[:NUMBER_OF_POSTS]
     context = {
-        'posts': posts,
-        'title': title
+        'posts': posts
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:10]
+    posts = group.posts.all()[:NUMBER_OF_POSTS]
     context = {
         'group': group,
         'posts': posts,
